d2478b642a30c465a2a2b688a32a18182f63da2e
Fedya <lilfedya@yandex.ru>
2025-12-12 22:59:25 +0300

    +PATCH

--- components/Like/Like.tsx
+++ components/Like/Like.tsx
@@ -1,26 +1,37 @@
 'use client'
 
-import { ButtonHTMLAttributes, useState } from 'react'
+import { ButtonHTMLAttributes, useEffect, useState } from 'react'
 import cn from 'classnames'
 import styles from './Like.module.css'
 import LikeIcon from './like.svg'
 
 interface LikeProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	isLiked: boolean
-	onToggle: () => void
+	postId: number
+	onLikeToggle: (postId: number, isLiked: boolean) => void
 }
 
-export const Like: React.FC<LikeProps> = ({ isLiked, onToggle, ...props }) => {
+export const Like: React.FC<LikeProps> = ({
+	isLiked,
+	onLikeToggle,
+	postId,
+	...props
+}) => {
 	const [isActive, setIsActive] = useState<boolean>(isLiked)
 
+	useEffect(() => {
+		setIsActive(isLiked)
+	}, [isLiked])
+
 	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-		event.preventDefault()
-		onToggle()
-		setIsActive((prev) => !prev)
+		const newState = !isActive
+		setIsActive(newState)
+		onLikeToggle(postId, newState)
 	}
 
 	return (
 		<button
+			type='button'
 			onClick={handleClick}
 			className={cn(styles.likeButton, {
 				[styles.liked]: isActive,
--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -3,17 +3,18 @@
 import { Like } from '../Like/Like'
 
 export const Post: React.FC = () => {
-	const handleToggleLike = async () => {
+	const handleToggleLike = async (postId: number, isLiked: boolean) => {
 		console.log('PATCH')
 		try {
-			await fetch('https://jsonplaceholder.typicode.com/posts/id', {
+			await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
 				method: 'PATCH',
 				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ liked: isLiked }),
 			})
 		} catch (e) {
 			// 3) откат при ошибке
 		}
 	}
 
-	return <Like onToggle={handleToggleLike} isLiked={false} />
+	return <Like onLikeToggle={handleToggleLike} postId={1} isLiked={false} />
 }
